fd62caec19aab01b91f513bb9c4235519be8dae8
Villabon12 <villabon.trujillo@gmail.com>
2024-09-29 14:18:39 -0500

    principal

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script>
   // @ts-ignore
-  import { Router, Route, Link } from 'svelte-routing';
+  import { Router, Route } from 'svelte-routing';
 
   // @ts-ignore
   import ProductDetail from "./views/admin/producto/+page.svelte";
@@ -8,7 +8,7 @@
   export let url = "";
 </script>
 <main>
-  <Link to="/product/">Productos</Link>
+  <a href="/product/">Productos</a>
   <Router url={url}>
     <Route path="/product/" component = {ProductDetail} />
   </Router>
